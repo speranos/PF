f34de98c63262322112b71dba1e5534793c7730b
speranos <aoueldma@student.1337.ma>
2024-09-17 01:16:03 +0100

    HomeInit

---
+++ front/src/app/show-case/show-case.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-case',
+  standalone: true,
+  imports: [],
+  templateUrl: './show-case.component.html',
+  styleUrl: './show-case.component.css'
+})
+export class ShowCaseComponent {
+
+}
