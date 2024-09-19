ecaaeb12b87ea33254e408483d6006d252117b79
speranos <aoueldma@student.1337.ma>
2024-09-19 03:30:53 +0100

    ShowCase-init

--- front/src/app/show-case/show-case.component.ts
+++ front/src/app/show-case/show-case.component.ts
@@ -1,17 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
-import { routes } from '../app.routes';
+import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
-import { P1Component } from './p1/p1.component';
-import { P2Component } from './p2/p2.component';
-import { P3Component } from './p3/p3.component';
-import { P4Component } from './p4/p4.component';
+
 
 @Component({
   selector: 'app-show-case',
   standalone: true,
-  imports: [FooterComponent, P1Component, P2Component, P3Component, P4Component],
+  imports: [FooterComponent, RouterModule],
   templateUrl: './show-case.component.html',
   styleUrl: './show-case.component.css'
 })
